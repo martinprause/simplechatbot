700da0fc1e5afc5d55e91af4c2c405e0b65e6127
Martin <martin.prause@whu.edu>
2023-11-10 12:42:23 +0100

    Chatbot ready

--- chatbot.py
+++ chatbot.py
@@ -7,4 +7,70 @@
 from openai import OpenAI
 import streamlit as st
 
-st.write("Hello World")
+import os
+APIKEY=os.environ["OPENAI_API_KEY"]
+
+
+client = OpenAI()
+
+class MyMessage:
+    def __init__(self, name, avatar, message):
+        self.name = name
+        self.avatar = avatar
+        self.message = message
+
+def showChatHistory():
+    if 'chat_history' in st.session_state:
+        for msg in st.session_state.chat_history:
+            st.chat_message(msg.name, avatar=msg.avatar).write(msg.message)
+
+
+def addMessage(msg):
+    if 'chat_history' not in st.session_state:
+        st.session_state.chat_history = []
+        st.session_state.chat_history.append(msg)
+    else:
+        st.session_state.chat_history.append(msg)
+
+
+def generateMessages(prompt):
+    messages=[]
+    if 'chat_history' in st.session_state:
+        for msg in st.session_state.chat_history:
+            m={"role": msg.name, "content": msg.message}
+            messages.append(m)
+    u={"role": "user", "content": prompt}
+    messages.append(u)
+    return messages
+
+def getBotResponse(prompt): 
+    
+    response = client.chat.completions.create(
+        model="gpt-4",
+        messages=generateMessages(prompt)
+    )
+    return response.choices[0].message.content
+
+
+def main():
+    if APIKEY is None:
+        st.write("Please set OPENAI_API_KEY environment variable.")
+        return
+    st.write("ChatBot")
+    reset = st.button("Reset")
+
+    if reset:
+        st.session_state.chat_history = []
+        st.experimental_rerun()
+
+    showChatHistory()
+    
+    prompt = st.chat_input("Say something nice to the bot!")
+
+    if prompt:
+        addMessage(MyMessage("user", "👩‍💻", prompt))
+        addMessage(MyMessage("assistant", "🤖", getBotResponse(prompt)))
+        st.experimental_rerun()
+
+if __name__ == "__main__":
+    main()
